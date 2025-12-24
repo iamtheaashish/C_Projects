e5c53d6a23a4ed4429b2c6704aa16f0e64a10629
Aashish Raj <iamtheaashish@gmail.com>
2025-12-24 11:55:13 +0530

    Refactor prime number program to accept user-defined range for prime number generation

--- Playgrounds/Prime_Number_Given_Range/main.c
+++ Playgrounds/Prime_Number_Given_Range/main.c
@@ -2,5 +2,20 @@
 we have to input minimum and maximum number by user*/
 #include <stdio.h>
 void main(){
-    int i,n,count=0;
+    int min,max,i,j,count;
+    printf("\nEnter Min and Max Value: ");
+    scanf("%d%d",&min,&max);
+    
+    for(i=min;i<=max;i++)
+    {
+        count=0;
+        for(j=1;j<=i;j++)
+        {
+            if(i%j==0){
+                count++;
+            }
+        }
+        if(count==2)
+            printf("\n%d is a Prime Number\n\t",i);
+    }
 }
