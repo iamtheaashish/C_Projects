c8836915536f76e2b56d35b41981626e0c762c43
Aashish Raj <iamtheaashish@gmail.com>
2025-12-24 21:50:22 +0530

    Add C program to generate multiplication table for a given number

---
+++ Playgrounds/Multiplication_Table/main.c
@@ -0,0 +1,12 @@
+/*Multiplication Table of any number from 1 to 10*/
+#include <stdio.h>
+void main(){
+    int n,i;
+    printf("Enter Number: ");
+    scanf("%d",&n);
+
+    for(i=1;i<=10;i++)
+    {
+        printf("%d*%d=%d\n",n,i,n*i);
+    }
+}
